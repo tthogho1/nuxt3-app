d94ca22df7f842d08bb27d1975312fd01b916d43
unknown <tthogho1@gmail.com>
2023-04-15 09:46:22 +0900

    bux fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,35 @@
-f<template>
+<template>
     <GoogleMap  ref="mapRef" api-key="AIzaSyAYemHqW9xU48b7KhMXauA6P0fDFTWyly0" style="width: 100%; height: 500px" :center="center" :zoom="15">
       <Marker :options="{ position: center }" />
     </GoogleMap>
   </template>
-  
-  
+    
 <script setup lang="ts">
 import { GoogleMap, Marker } from "vue3-google-map";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
 const mapRef = ref(null);
-let center = { lat: 0, lng: 0 };
-onMounted(() => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
+const center = ref({ lat: 0, lng: 0 }); // first position
+const getLocation = function(map:any,center:any){
+    navigator.geolocation.getCurrentPosition(
             // success callback
             function(position) {
                 const lat = position.coords.latitude;
                 const lng = position.coords.longitude;
                 console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-                mapRef.value.map.panTo({ lat, lng });
-                center = { lat, lng };    
+                map.panTo({ lat, lng });
+                center.value = { lat: lat, lng: lng };
             },
             // error callback
             function(error) {
                 console.error(`Error getting location: ${error.message}`);
             }
-        );
-    }
-});
+    );
+} 
 
-// const center = { lat: 40.689247, lng: -74.044502 };
+watch(() => mapRef.value?.ready, (ready) => {
+    if (!ready) return;
+    const map = mapRef.value.map;
+    getLocation(map,center);
+})
 </script>
