c03ddfc6eab0f0781147e39f0546d12981157a89
unknown <tthogho1@gmail.com>
2023-09-30 21:10:15 +0900

    fix path to image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,13 +12,19 @@ export default defineNuxtConfig({
         public: {
             // This is the public key of the server
             // You can find it in the Realm Dashboard
+            METAL_CLIENT_ID: 'ci_Klqqk9LpWdWUqA7EO2zlSYrTWLNHyRVGWXUD23Wf0pQ=',
+            METAL_SEARCHE_URL: 'https://api.getmetal.io/v1/search',
+            METAL_IMAGE_INDEX: '650fbf41ded6e7b8b47766d8',
+            METAL_API_KEY: 'pk_jEyG+TzAS1czHqStsWJRdEghxfz+rvfpNdBzZ+mY/OU=',
+            
             GOOGLE_MAPS_API_KEY: 'AIzaSyAYemHqW9xU48b7KhMXauA6P0fDFTWyly0',
             RESEMBLE_PIC_LAMBDA :'https://77vd6pifbj5z2c2lvjbkuftzaa0lpozm.lambda-url.ap-northeast-1.on.aws/',
-            MONGODB_ATLAS_TOKEN_INTERVAL : 1200000,
             SUPABASE_URL : 'https://mzojgijectnqzezyfndy.supabase.co',
             SUPABASE_PUBLIC_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16b2pnaWplY3RucXplenlmbmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0MDA1MDQsImV4cCI6MjAwOTk3NjUwNH0.ZoQGWQByUgglnfGCFCF1lmi0DKjs4JGPPhFx_nPrxeA',
 
-            // REALM_PUBLIC_KEY: 'zltDLjGDHqJHzQ0tSHA3XSZJUTnV5TxBmjW2PopKInszFsDxqSAEubmtq5tRRLgm'
+            MONGODB_ATLAS_TOKEN_INTERVAL : 1200000,
+            MONGODB_ATLAS_GRAPHQL_URL: 'https://realm.mongodb.com/api/client/v2.0/app/webcamql-qrkjj/graphql',
+            MONGODB_PUBLIC_KEY: 'zltDLjGDHqJHzQ0tSHA3XSZJUTnV5TxBmjW2PopKInszFsDxqSAEubmtq5tRRLgm'
         }
     }
 })
