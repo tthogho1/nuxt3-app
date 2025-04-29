ae34fe87fd9486cfdb5a1147ba266b65d833a66f
t-tougou <t-tougou@ndics.co.jp>
2025-04-29 21:43:47 +0900

    Change get Countries from MongoDB to Hasura

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  css: ["bootstrap/dist/css/bootstrap.min.css",
-      "bootstrap-icons/font/bootstrap-icons.css"],
+  css: ['bootstrap/dist/css/bootstrap.min.css', 'bootstrap-icons/font/bootstrap-icons.css'],
 
   ssr: true,
 
   app: {
-      baseURL: '/'
+    baseURL: '/',
   },
 
-  modules: ['@pinia/nuxt','@nuxt/test-utils/module'],
+  modules: ['@pinia/nuxt', '@nuxt/test-utils/module'],
 
   runtimeConfig: {
-      metalClientId:'',
-      metalSearcheUrl:'',
-      metalImageIndex:'',
-      metalApiKey:'',
-      resemblePicLambda:'',
-      mongodbAtlasToken:'',
-      mongodbAtlasGraphqlUrl:'',
-      mongodbKey:'',
-      mongodbAtlasGraphqlCountryUrl:'',
+    metalClientId: '',
+    metalSearcheUrl: '',
+    metalImageIndex: '',
+    metalApiKey: '',
+    resemblePicLambda: '',
+    mongodbAtlasToken: '',
+    mongodbAtlasGraphqlUrl: '',
+    mongodbKey: '',
+    mongodbAtlasGraphqlCountryUrl: '',
+    hasuraGraphqlAdminSecret: '',
+    hasuraGraphqlUrl: '',
 
-      public: {
-          googleMapsApiKey:'',
-          supabaseUrl:'',
-          supabaseKey:'',
-          translateApi:'',
-          imageServer:'',
-          mongodbKey:'',
-          MONGODB_ATLAS_TOKEN_INTERVAL : 1200000,
-      }
+    public: {
+      googleMapsApiKey: '',
+      supabaseUrl: '',
+      supabaseKey: '',
+      translateApi: '',
+      imageServer: '',
+      mongodbKey: '',
+      MONGODB_ATLAS_TOKEN_INTERVAL: 1200000,
+    },
   },
 
-  compatibilityDate: "2024-07-18"
-})
+  compatibilityDate: '2024-07-18',
+});
--- server/api/getWebCams.ts
+++ server/api/getWebCams.ts
@@ -1,34 +1,31 @@
 const config = useRuntimeConfig();
 
+export default defineEventHandler(async event => {
+  let body;
+  try {
+    body = await readBody(event);
+  } catch (e) {
+    console.log(e);
+  }
+  // body: { query : queryMsg}
 
+  const url = config.mongodbAtlasGraphqlUrl;
+  const queryMsg = body.query;
 
-export default defineEventHandler(async (event) => {
-    let body ;
-    try{
-        body = await readBody(event);
-    }catch(e){
-        console.log(e);
-    }
-    // body: { query : queryMsg}
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      apiKey: config.mongodbKey,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: queryMsg,
+    }),
+  });
 
-    const url = config.mongodbAtlasGraphqlUrl;
-	const queryMsg = body.query;
+  if (!response.ok) {
+    throw new Error('Network response was not ok.');
+  }
 
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'apiKey': config.mongodbKey,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            query: queryMsg
-        })
-	})
-
-    if (!response.ok) {
-        throw new Error('Network response was not ok.');
-    }
-
-
-    return await response.json();
+  return await response.json();
 });
